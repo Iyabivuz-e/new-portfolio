6a23d0881f1a3bc6180c60161d669d0371e4782f
Dieudonne <iyabivuzed@gmail.com>
2025-03-06 22:19:52 +0100

    updated the home section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Skills from "./components/Skills/Skills";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="overflow-hidden">
       <Hero/>
       <About/>
       <Skills/>
